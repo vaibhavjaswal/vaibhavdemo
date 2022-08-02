ad548f50987762bf01c6cdd4b79e2b1710abb035
vaibhavjaiswal <vaibhavjaiswal815@gmail.com>
2022-08-02 18:49:01 +0530

    change a sum digit

--- multi.java
+++ multi.java
@@ -2,9 +2,7 @@ public class multi {
     public static void main(String args[]){
 
         int a=8;
-        int b=1;
-        int c= a+b;
-        System.out.print("hii");
+        int b=12;
         System.out.print(a*b);
     }
 }
