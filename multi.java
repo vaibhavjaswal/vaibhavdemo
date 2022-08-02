1c13fbef44015c03fb13d3e293c13ad575a6a0f5
vaibhavjaiswal <vaibhavjaiswal815@gmail.com>
2022-08-02 17:35:44 +0530

    add multi

---
+++ multi.java
@@ -0,0 +1,10 @@
+public class multi {
+    public static void main(String args[]){
+
+        int a=8;
+        int b=1;
+        int c= a+b;
+        System.out.print("hii");
+        System.out.print(a*b);
+    }
+}
--- sum.java
+++ sum.java
@@ -4,6 +4,7 @@ public static void main(String args[]){
     int a=8;
     int b=1;
     int c= a+b;
-    System.out.print("hiii");
+    
+    System.out.print(c);
       
 }}
